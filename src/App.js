025e90138029669a675f993e924a45553c496cd3
surajack12 <58561274+surajack12@users.noreply.github.com>
2020-04-30 20:08:03 +0530

    added index.html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+constructor(props){
+    super(props);
+    this.state = {name:"suraj"};
+}
+    render(){
+  
+  
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <h1><code onMouseOver={()=>{this.setState({name:"ravi"})}}>{this.state.name}</code></h1>
+  <h1><code onMouseOver={()=>{this.setState({name:"suraj"})}}>{this.state.name}</code></h1>
+
       </header>
+      <div></div>
     </div>
   );
 }
-
+}
 export default App;
